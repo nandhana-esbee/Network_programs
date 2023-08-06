505ec0164d1fde5d86e80b645dd1ca58d84e4fc4
Nandhana SB <nandhanaesbee16@gmail.com>
2023-08-06 19:29:53 +0530

    deleted unwanted leakage codes

--- leakybucket.c
+++ leakybucket.c
@@ -1,27 +1,5 @@
 #include<stdio.h>
 int main(){
-   /** int incoming, outgoing,buck_size,n,store=0;
-    printf("Enter bucket size, outgoing rate and no of inputs: ");
-    scanf("%d %d %d" ,&buck_size,&outgoing,&n);
-    while(n!=0)
-    {
-        printf("Enter the incoming packet size :");
-        scanf("%d" ,&incoming);
-        printf("Incoming packet size %d\n" ,incoming);
-        if(incoming <=(buck_size-store)){
-            store +=incoming;
-            printf("Bucket buffer size %d out of %d\n", store,buck_size);
-        }
-        else{
-            printf("Dropped %d no of packets\n", incoming-(buck_size-store));
-            printf("Bucket buffer size %d out of %d\n" ,store,buck_size);
-            store=buck_size;
-        }
-        store=store-outgoing;
-        printf("After outgoing %d packets left out of %d in buffer\n" ,store,buck_size);
-        n--;
-    }*/
-
     //Simple leaky bucket algorithm
     int storage=0, no_of_ip,op_rate,buck_size,ip_size,size_left;
     printf("Enter the no of inputs :");
